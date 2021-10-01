7a13d38a54941ae2c369c48a50a1e67c7cf08cf6
Wyatt Hart <wyatt.c.hart@gmail.com>
2021-09-30 19:53:08 -0700

    Added functional submit button with prompt to verify if user wants to submit, used localStorage to remember previous order and added prompts to verify if user wants to continue from previous order or clear previous selections

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,10 @@ let gbTotal = document.querySelector('#qty-gb');
 let ccTotal = document.querySelector('#qty-cc');
 let sugarTotal = document.querySelector('#qty-sugar')
 let cookieTotal = document.querySelector('#qty-total')
+let divSummary = document.querySelector('.summary')
+
+let submitChoices = document.createElement('button')
+submitChoices.textContent = 'Submit'
 
 // First, tell us your name
 let yourName = "Wyatt Clay Hart"
@@ -15,6 +19,63 @@ let ct = 0      // Cookie Total
 document.getElementById('minus-gb').style.backgroundColor = 'darkGrey'
 document.getElementById('minus-cc').style.backgroundColor = 'darkGrey'
 document.getElementById('minus-sugar').style.backgroundColor = 'darkGrey'
+
+let serializedGb = parseInt(localStorage.getItem('gb'))
+let serializedCc = parseInt(localStorage.getItem('cc'))
+let serializedSugar = parseInt(localStorage.getItem('sugar'))
+let serializedCt = parseInt(localStorage.getItem('ct'))
+
+if( serializedGb > 0 || serializedCc > 0 || serializedSugar > 0){
+    let resume = window.prompt('It looks like you still deciding on what cookies you wanted, would you like to pickup where you left off? \n\r [Y] - [N]');
+    if(resume == 'Y'){
+        window.alert("OK, please let us know what else you'd like!")
+    }else if (resume == 'N'){
+        localStorage.removeItem('gb')
+        localStorage.removeItem('cc')
+        localStorage.removeItem('sugar')
+        localStorage.removeItem('ct')
+        gb = 0
+        cc = 0
+        sugar = 0
+        ct = 0
+        serializedGb = 0
+        serializedCc = 0
+        serializedSugar = 0
+        serializedCt = 0
+        gbTotal.textContent = gb
+        ccTotal.textContent = cc
+        sugarTotal.textContent = sugar
+        cookieTotal.textContent = ct
+        window.alert('OK, your decisions from last time were cleared. If you would like us to make you some cookies, let us know!')
+        }
+}
+
+if(serializedGb > 0){
+    gb = serializedGb
+    ct = serializedCt
+    gbTotal.textContent = gb
+    cookieTotal.textContent = ct
+    document.getElementById('minus-gb').style.backgroundColor = 'burlywood'
+}
+if(serializedCc > 0){
+    cc = serializedCc
+    ct = serializedCt
+    ccTotal.textContent = cc
+    cookieTotal.textContent = ct
+    document.getElementById('minus-cc').style.backgroundColor = 'burlywood'
+}
+if(serializedSugar > 0){
+    sugar = serializedSugar
+    ct = serializedCt
+    sugarTotal.textContent = sugar
+    cookieTotal.textContent = ct
+    document.getElementById('minus-sugar').style.backgroundColor = 'burlywood'
+}
+if(serializedCt > 0){
+    ct = serializedCt
+    gbTotal.textContent = gb
+    cookieTotal.textContent = ct
+}
 
 
 
@@ -35,6 +96,8 @@ document.getElementById('add-gb').addEventListener('click', function() {
     if (gb > 0){
         document.getElementById('minus-gb').style.backgroundColor = 'burlywood'
     }
+    localStorage.setItem('gb', gb)
+    localStorage.setItem('ct', ct)
 })
 document.getElementById('add-cc').addEventListener('click', function() {
     // HINT: You can delete this console.log after you no longer need it!
@@ -46,7 +109,8 @@ document.getElementById('add-cc').addEventListener('click', function() {
     if (cc > 0){
         document.getElementById('minus-cc').style.backgroundColor = 'burlywood'
     }
-    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
+    localStorage.setItem('cc', cc)
+    localStorage.setItem('ct', ct)
 })
 document.getElementById('add-sugar').addEventListener('click', function() {
     // HINT: You can delete this console.log after you no longer need it!
@@ -58,7 +122,8 @@ document.getElementById('add-sugar').addEventListener('click', function() {
     if (sugar > 0){
         document.getElementById('minus-sugar').style.backgroundColor = 'burlywood'
     }
-    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
+    localStorage.setItem('sugar', sugar)
+    localStorage.setItem('ct', ct)
 })
 
 
@@ -74,7 +139,8 @@ document.getElementById('minus-gb').addEventListener('click', function() {
     }
     gbTotal.textContent = gb
     cookieTotal.textContent = ct
-
+    localStorage.setItem('gb', gb)
+    localStorage.setItem('ct', ct)
 })
 document.getElementById('minus-cc').addEventListener('click', function() {
     console.log('Chocolate Chip - button was clicked!')
@@ -87,6 +153,8 @@ document.getElementById('minus-cc').addEventListener('click', function() {
     }
     ccTotal.textContent = cc
     cookieTotal.textContent = ct
+    localStorage.setItem('cc', cc)
+    localStorage.setItem('ct', ct)
 })
 document.getElementById('minus-sugar').addEventListener('click', function() {
     console.log('Sugar Sprinkle - button was clicked!')
@@ -99,4 +167,40 @@ document.getElementById('minus-sugar').addEventListener('click', function() {
     }
     sugarTotal.textContent = sugar
     cookieTotal.textContent = ct
+    localStorage.setItem('sugar', sugar)
+    localStorage.setItem('ct', ct)
+})
+
+divSummary.append(submitChoices)
+
+submitChoices.addEventListener('click', function(){
+    if(ct > 0){
+        let verifySubmission = window.prompt('Are you sure you would like to submit your choices? \r\n[Y] - [N]')
+        if(verifySubmission == 'Y'){
+            localStorage.removeItem('gb')
+            localStorage.removeItem('cc')
+            localStorage.removeItem('sugar')
+            localStorage.removeItem('ct')
+            gb = 0
+            cc = 0
+            sugar = 0
+            ct = 0
+            serializedGb = 0
+            serializedCc = 0
+            serializedSugar = 0
+            serializedCt = 0
+            gbTotal.textContent = gb
+            ccTotal.textContent = cc
+            sugarTotal.textContent = sugar
+            cookieTotal.textContent = ct
+            document.getElementById('minus-gb').style.backgroundColor = 'darkGrey'
+            document.getElementById('minus-cc').style.backgroundColor = 'darkGrey'
+            document.getElementById('minus-sugar').style.backgroundColor = 'darkGrey'
+            window.alert('OK, your decisions have been submitted. We will let you know when your cookies are ready!')
+        }else if(verifySubmission == 'N'){
+            window.alert("No worries, let us know when you're ready to finalize your order!")
+        }
+    }else if(ct == 0){
+        window.alert('You need to have selected cookies in order to order them, silly!')
+    }
 })
